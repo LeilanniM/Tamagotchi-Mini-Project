7ede4169ab81f048a1bef021f4a48feca9a9586c
lmakoske1 <leilanni.dev@gmail.com>
2022-07-06 10:10:30 -0700

    bainstorming best approach for Pets life cycle

--- app.js
+++ app.js
@@ -24,16 +24,18 @@ class Pet {
   };
 }
 
+//INSTANTIATE pet:
+
+const dino = new Pet("Pol");
+console.log(dino);
+
+//====================================================================
+
 //first it is born/hatched.
 //then it requires to be fed <-- every 4 minutes
 //then it requires to play <-- every 4 minutes
 //and then it requires to sleep <-- every 4 minutes
 //it ages <--- every 5 minutes
-
-//INSTANTIATE pet:
-
-const dino = new Pet("Pol");
-console.log(dino);
 
 //====================================
 
@@ -63,4 +65,9 @@ console.log(dino);
 
 //If Im going to pass a class method, that references the class itself, in other words 'this' as a call back function then you must wrap it in an annonymous function otherwise 'this' will reference the object that setInterval is living in.
 
-//=============================
+//====================================================================
+
+//Q: how do I make each action trigger the other? so that it doesnt require all actions at the same time
+
+//A: I can put them inside a bigger function that gets triggered when the button START is pressed, and inside this function we can create if statments like, ONLY if it is hatched can it asks to be fed, and ONLY if its been FED can it ask to play, and ONLY if it has PLAYED can it ask to go to SLEEP. and after SLEEP it should loop? all over again.
+//I could create an array of functions too, not sure if it would work while looping throught it.... maybe it can loop and have IF statements inside
