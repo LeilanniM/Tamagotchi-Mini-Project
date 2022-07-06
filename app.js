8df796d28ada123c978c89b8ff8c0e68e05456c9
lmakoske1 <leilanni.dev@gmail.com>
2022-07-06 12:07:44 -0700

    FEED ME/ FEED IT

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//------------------------->TAMAGOTCHI<-----------------------------
+
+//====================================================================
+//PET:
 class Pet {
   constructor(name) {
     this.name = name;
@@ -6,68 +10,89 @@ class Pet {
     this.boredom = 1;
     this.age = -1;
   }
-  feedPet = () => {
-    console.log("nibble nibble");
-  };
-  lightSwitch = () => {
-    console.log("time to sleep");
-  };
-  playWithPet = () => {
-    console.log("Play time!");
-  };
   ageUp = () => {
     if (this.age === 0) {
       console.log(`It hatched!`);
     }
     this.age = this.age + 1;
     console.log(`${this.name} just turned ${this.age}`);
-  };
-}
+  }; //end of function
+
+  //Pet Needs
+
+  feedMe = () => {
+    this.hunger += 1;
+    console.log(`HUNGER: ${this.hunger}`);
+
+    if (this.hunger >= 5 && this.hunger < 10) {
+      console.log(`Feed Me`);
+    }
+    if (this.hunger === 10) {
+      console.log(`Im dead girl`);
+      clearInterval(countHunger);
+    }
+  }; //end of function
+} //end of class
+
+//====================================================================
+
+//====================================================================
+//OWNER:
+
+//Pet Owner duties:
+
+feedPet = () => {
+  if (dino.hunger < 10) {
+    dino.hunger = dino.hunger - 2;
+    console.log(`nibble nibble`);
+    console.log(`HUNGER: ${dino.hunger}`);
+    console.log(`burp`);
+  } else {
+    console.log(`it's too late to apologize`);
+  }
+};
+lightSwitch = () => {
+  console.log("time to sleep");
+};
+playWithPet = () => {
+  console.log("Play time!");
+};
 
 //INSTANTIATE pet:
 
 const dino = new Pet("Pol");
 console.log(dino);
+//====================================================================
+
+//===================================================================
+//HOW TO CALL A CLASS FUNCTION INSIDE A CALLBACK FUNCTION:
+//If Im going to pass a class method, that references the class itself, in other words 'this' as a call back function then you must wrap it in an annonymous function otherwise 'this' will reference the object that setInterval is living in.
+//====================================================================
 
 //====================================================================
 
+//LIFE CYCLE
 //first it is born/hatched.
-//then it requires to be fed <-- every 4 minutes
+//then it get hungry <-- every 4 minutes
 //then it requires to play <-- every 4 minutes
 //and then it requires to sleep <-- every 4 minutes
 //it ages <--- every 5 minutes
+//====================================================================
 
-//====================================
+//====================================================================
+//------------------------>FEED ME/FEED IT<---------------------------
 
-//FEED TIME:
+//We can Set a timer that starts INCREASING THE HUNGER line
 
-// setInterval(dino.feedPet, 240000);
+const countHunger = setInterval(dino.feedMe, 3000); //this will ask to feed it every X seconds
+//make it stop counting after 10 --> clearSetInterval
 
-//====================================
+//Q: How do I feed it?
+//A: By calling the function feedPet() inside the console.
 
-//PLAY TIME:
-
-// setInterval(dino.playWithPet, 240000) //every 4 minutes
-
-//====================================
-
-//SLEEP TIME:
-
-// setInterval(dino.lightSwitch, 240000);
-
-//====================================
-
-//AGING:
-
-// setInterval(dino.ageUp, 240000);
-
-//====================================
-
-//If Im going to pass a class method, that references the class itself, in other words 'this' as a call back function then you must wrap it in an annonymous function otherwise 'this' will reference the object that setInterval is living in.
+//Q: it is still letting me feed it after it is dead
+//A: create 'if statement' so that the function: feedPet() only runs if pet is alive!
 
 //====================================================================
 
-//Q: how do I make each action trigger the other? so that it doesnt require all actions at the same time
-
-//A: I can put them inside a bigger function that gets triggered when the button START is pressed, and inside this function we can create if statments like, ONLY if it is hatched can it asks to be fed, and ONLY if its been FED can it ask to play, and ONLY if it has PLAYED can it ask to go to SLEEP. and after SLEEP it should loop? all over again.
-//I could create an array of functions too, not sure if it would work while looping throught it.... maybe it can loop and have IF statements inside
+//====================================================================
