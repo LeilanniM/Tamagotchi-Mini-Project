c5c876a00fd199b60f363111f75795c3773a76c5
lmakoske1 <leilanni.dev@gmail.com>
2022-07-06 10:03:01 -0700

    Feed, Play, Sleep and Aging have been added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const ageRate = 180000;
-
 class Pet {
   constructor(name) {
     this.name = name;
@@ -8,19 +6,15 @@ class Pet {
     this.boredom = 1;
     this.age = -1;
   }
-  feedPet() {
+  feedPet = () => {
     console.log("nibble nibble");
-  }
-  lightSwitch() {
+  };
+  lightSwitch = () => {
     console.log("time to sleep");
-  }
-  playWithPet() {
+  };
+  playWithPet = () => {
     console.log("Play time!");
-  }
-  // ageUp = () => {
-  //   this.age = this.age + 1;
-  //   console.log(`${this.name} just turned ${this.age}`);
-  // };
+  };
   ageUp = () => {
     if (this.age === 0) {
       console.log(`It hatched!`);
@@ -30,15 +24,43 @@ class Pet {
   };
 }
 
-//what do I want first? --> Instantiate a new pet
+//first it is born/hatched.
+//then it requires to be fed <-- every 4 minutes
+//then it requires to play <-- every 4 minutes
+//and then it requires to sleep <-- every 4 minutes
+//it ages <--- every 5 minutes
+
+//INSTANTIATE pet:
 
 const dino = new Pet("Pol");
 console.log(dino);
 
-setInterval(dino.ageUp, ageRate * 1000);
+//====================================
 
-//Now what do we want? --> to age every 3 seconds // for birthday function to be called every 3 seconds and STOP if/when pet dies
+//FEED TIME:
+
+// setInterval(dino.feedPet, 240000);
+
+//====================================
+
+//PLAY TIME:
+
+// setInterval(dino.playWithPet, 240000) //every 4 minutes
+
+//====================================
+
+//SLEEP TIME:
+
+// setInterval(dino.lightSwitch, 240000);
+
+//====================================
+
+//AGING:
+
+// setInterval(dino.ageUp, 240000);
+
+//====================================
 
 //If Im going to pass a class method, that references the class itself, in other words 'this' as a call back function then you must wrap it in an annonymous function otherwise 'this' will reference the object that setInterval is living in.
 
-// clearInterval(timerId);
+//=============================
