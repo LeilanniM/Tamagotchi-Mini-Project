a44aafabf6a71432d476f5f47c18b68785639154
lmakoske1 <leilanni.dev@gmail.com>
2022-07-06 09:23:59 -0700

    Dino is now aging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const ageRate = 180000;
+
 class Pet {
   constructor(name) {
     this.name = name;
     this.hunger = 1;
     this.sleepiness = 1;
     this.boredom = 1;
-    this.age = 0;
+    this.age = -1;
   }
   feedPet() {
     console.log("nibble nibble");
@@ -15,10 +17,17 @@ class Pet {
   playWithPet() {
     console.log("Play time!");
   }
-  ageUp() {
-    this.age += 1;
-    console.log(`${this.age}`);
-  }
+  // ageUp = () => {
+  //   this.age = this.age + 1;
+  //   console.log(`${this.name} just turned ${this.age}`);
+  // };
+  ageUp = () => {
+    if (this.age === 0) {
+      console.log(`It hatched!`);
+    }
+    this.age = this.age + 1;
+    console.log(`${this.name} just turned ${this.age}`);
+  };
 }
 
 //what do I want first? --> Instantiate a new pet
@@ -26,6 +35,10 @@ class Pet {
 const dino = new Pet("Pol");
 console.log(dino);
 
-//Now what do we want? --> to age every 1 minute// for birthday function to be called every minute and STOP if/when pet dies
+setInterval(dino.ageUp, ageRate * 1000);
 
-const callAgeUp = setInterval(dino.ageUp, 3000);
+//Now what do we want? --> to age every 3 seconds // for birthday function to be called every 3 seconds and STOP if/when pet dies
+
+//If Im going to pass a class method, that references the class itself, in other words 'this' as a call back function then you must wrap it in an annonymous function otherwise 'this' will reference the object that setInterval is living in.
+
+// clearInterval(timerId);
