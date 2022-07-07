d35db68cfb42caf328b6c5cde1ac840741f2f1dc
lmakoske1 <leilanni.dev@gmail.com>
2022-07-06 21:01:47 -0700

    started all over on sanbox since I got tangled

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,6 @@
 //=========================TAMAGOTCHI============================
+// let ageCount;
+// let hungerCount;
 
 class Pet {
   constructor(name) {
@@ -7,15 +9,51 @@ class Pet {
     this.sleepiness = 1;
     this.boredom = 1;
     this.age = 0;
+    this.isDead = false;
   } // End of constructor
-}
+
+  hatch = () => {
+    state.age = this.age;
+    console.log(`I've hatched!`);
+    state.hatched = true;
+    state.ageCount = setInterval(this.ageUp, 5000);
+    state.hungerCount = setInterval(this.getsHungry, 2000);
+  };
+
+  ageUp = () => {
+    this.age += 1;
+    state.age = this.age;
+
+    console.log(`${this.name} just turned ${this.age}`);
+  };
+
+  getsHungry = () => {
+    this.hunger += 1;
+    state.hunger = this.hunger;
+
+    console.log(`HUNGER: ${this.hunger}`);
+
+    if (this.hunger === 7) {
+      console.log("Girl I'm dead and hungry...");
+      this.isDead = true;
+      clearInterval(state.ageCount);
+      clearInterval(state.hungerCount);
+      console.log(state);
+    } else if (this.hunger >= 5) {
+      console.log(`feed me, feed me, FEED MEEEEE!!!`);
+    }
+
+    // console.log(state);
+  };
+  //-------------------
+} //end of class
 
 //===========================INSTANTIATE=================================
 
 const startGame = () => {
   const dino = new Pet("Pol");
-  console.log(dino);
   console.log(`${dino.name} has been instantiated`);
+  setTimeout(dino.hatch, 5000);
 };
 
 //===============================STATE================================
@@ -26,11 +64,16 @@ const state = {
   sleepiness: null,
   boredom: null,
   age: null,
+  hatched: false,
+  //------------------ Below: to be able to access these variables from anywhere.
+  hungerCount: null,
+  ageCount: null,
 };
+// console.log(state);
 
-//=======================START THE GAME=========================
+//==========================START THE GAME===========================
 
 const startButton = document.querySelector("#start-pause");
 startButton.addEventListener("click", startGame);
 
-//=============================================================
+//===================================================================
